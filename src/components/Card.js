f07caa3653bd1e8d82592cf849d360c95117a9e8
Doruk <doruxa@gmail.com>
2020-04-20 09:31:51 +0300

    pwa

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({item}) => {
     const {name, email} = item
     return(
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${name}?200x200`} alt='RobotImg' />
+            <img src={`https://robohash.org/${name}?size=s200x200`} alt='RobotImg' />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,12 @@
 import React from 'react'
 
-const SearchBox = ({onSearchChange}) => {
-    return (
-        <div className='pa2'>
-            <input
-                className='pa3 ba b--green bg-lightest-blue'
-                type='search'
-                placeholder='Search Robots'
-                onChange={onSearchChange}
-            />
-        </div>
+const SearchBox = ({ onSearchChange }) => {
+    return (<div className='pa2' >
+        <input aria-label='Search Robots'
+            className='pa3 ba b--green bg-lightest-blue'
+            type='search'
+            placeholder='Search Robots'
+            onChange={onSearchChange} /> </div>
     )
 }
 
